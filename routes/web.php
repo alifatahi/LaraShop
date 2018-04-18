6e69f225d0cd1abe209a9dc5c89001a1145a9b71
Ali Fatahi <a.fatahi1992@gmail.com>
2018-04-18 12:19:54 +0430

    Hashes & Cache in Redis

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,42 @@
 <?php
 
-//Our Counter for Videos Downloads(it tell us about count of downloaded video)
-Route::get('videos/{id}', function ($id) {
-//    We Namespacing our download with . and pass id to that
-    $downloads = Redis::get("videos.{$id}.downloads");
+Route::get('/', function () {
+//    Get all Hashes
+//    return Redis::hgetall('user.1.stats');
 
-    return view("Welcome", compact('downloads'));
+//    Put Cache "Key" , "Value" , Time of cache
+    Cache::put('foo', ['name' => 'Ali', 'age' => 3], 10);
+
+    return Cache::get('foo');
 });
 
-//Download link
-Route::get('videos/{id}/download', function ($id) {
-//        Prepare Download
+// For Increment Favorite for User 1
+//Route::get('favorite-video', function () {
+////    increment
+//    Redis::hincrby('user.1.stats', 'favorites', 1);
+//
+//    return redirect('/');
+//
+//});
 
-//    Increment Download Time
-    Redis::incr("videos.{$id}.downloads");
 
-    return back();
-});
+////Our Counter for Videos Downloads(it tell us about count of downloaded video)
+//Route::get('videos/{id}', function ($id) {
+////    We Namespacing our download with . and pass id to that
+//    $downloads = Redis::get("videos.{$id}.downloads");
+//
+//    return view("Welcome", compact('downloads'));
+//});
+//
+////Download link
+//Route::get('videos/{id}/download', function ($id) {
+////        Prepare Download
+//
+////    Increment Download Time
+//    Redis::incr("videos.{$id}.downloads");
+//
+//    return back();
+//});
 
 
 
