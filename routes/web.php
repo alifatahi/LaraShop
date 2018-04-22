d8a4ed6cf69876fbba701c3e45af355a98346d16
Ali Fatahi <a.fatahi1992@gmail.com>
2018-04-22 23:42:25 +0430

    Fix Master and Make Layouts

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,13 @@ Route::get('/', function () {
     return view('front.home');
 });
 
+Route::get('shop', 'HomeController@getShop');
 
 
 
 
+
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
