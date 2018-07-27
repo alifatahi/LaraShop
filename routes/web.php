806ca7c96af1532fdcd76e80b989c8a25c0ba8e4
Ali Fatahi <a.fatahi1992@gmail.com>
2018-07-27 14:20:29 +0430

    New Start for Larashop :) Change Theme and add will add new functionality

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,10 @@
 <?php
 
-//Index
-Route::get('/', 'HomeController@index')->name('home');
+Route::get('/', 'LandingPageController@index')->name('landing-page');
 
+Route::get('/shop', 'ShopController@index')->name('shop.index');
+Route::get('/shop/{product}', 'ShopController@show')->name('shop.show');
 
-Route::get('shop', 'HomeController@getShop')->name('shop');
-Route::get('contact', 'HomeController@getContact')->name('contact');
-
-Auth::routes();
-
-
-//Admin
-Route::get('admin', 'AdminController@index')->name('admin');
-Route::post('admin/add_product', 'AdminController@store')->name('add_product');
-
+Route::view('/cart', 'cart');
+Route::view('/checkout', 'checkout');
+Route::view('/thankyou', 'thankyou');
